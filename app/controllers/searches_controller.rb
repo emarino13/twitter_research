a1e5ba3eb1a4303f90bd64fbf080bb38f3acfaa9
Elaine Marino <elaine.c.marino@gmail.com>
2012-06-27 11:43:02 -0600

    Class Twitter Project revsied in class 6_27

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,13 @@
 class SearchesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    search = SimpleTwitter::Search.new
+    @results=search.search(params[:q])
+  end
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TwitterResearch::Application.routes.draw do
 
   root(:to => 'searches#index') #setting up root index action
+  resources(:searches)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
