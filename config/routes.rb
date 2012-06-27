26037b6e4e5d4661ae35e886feb88e70f008a109
Elaine Marino <elaine.c.marino@gmail.com>
2012-06-27 09:42:32 -0600

    New Twitter_Research files done in class 6_27

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TwitterResearch::Application.routes.draw do
+
+  root(:to => 'searches#index') #setting up root index action
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
